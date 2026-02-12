8ac77b3e32cbd1885e54bbf53430671749423abf
advaithProjects <advaithvijjagiri@gmail.com>
2026-02-12 20:30:19 +0530

    12/02/2026 8:30pm

---
+++ src/firebase.js
@@ -0,0 +1,62 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvX5ddf6nO6SCw9OgsFLgXG9zYNtBXWtA",
+  authDomain: "netflix-clone-4adb8.firebaseapp.com",
+  databaseURL:
+    "https://netflix-clone-4adb8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "netflix-clone-4adb8",
+  storageBucket: "netflix-clone-4adb8.firebasestorage.app",
+  messagingSenderId: "863100880117",
+  appId: "1:863100880117:web:2aa3bfa537b0e7dc5871bc",
+  measurementId: "G-TRHZ2P703R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+// User SignUp function
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+};
+// User SignIn function
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+};
+// User LogOut function
+const logout = () => {
+  signOut(auth);
+};
+export { auth, db, login, signup, logout };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
+import { login, signup } from "../../firebase";
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const user_auth = async (event) => {
+    event.preventDefault();
+    if (signState == "sign In") {
+      await login(email, password);
+    } else {
+      await signup(name, email, password);
+    }
+  };
 
   return (
     <div className="login">
@@ -13,13 +26,36 @@ const Login = () => {
         <h1>{signState}</h1>
         <form>
           {signState === "Sign Up" ? (
-            <input type="text" placeholder="Your Name" />
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              placeholder="Your Name"
+            />
           ) : (
             ""
           )}
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button>{signState}</button>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="Password"
+          />
+          <button onClick={user_auth} type="submit">
+            {signState}
+          </button>
 
           <div className="form-help">
             <div className="remember">
