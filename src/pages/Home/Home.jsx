75f2b1ba457c63d6170b81a2426a4de58766c742
advaithProjects <advaithvijjagiri@gmail.com>
2026-02-13 17:41:27 +0530

    13/02/2026 5:40pm

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
-import play_icon from "../../assets/Play_icon.png";
+import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
 import Titlecards from "../../components/Titlecards/Titlecards";
 import Footer from "../../components/Footer/Footer";
@@ -35,9 +35,9 @@ const Home = () => {
         </div>
       </div>
       <div className="more-cards">
-        <Titlecards title={"Blockbuster on Netflix"} category={"top_rated"}/>
+        <Titlecards title={"Blockbuster on Netflix"} category={"top_rated"} />
         <Titlecards title={"Only on Netflix"} category={"popular"} />
-        <Titlecards title={"Upcoming on Netflix"} category={"upcoming"}/>
+        <Titlecards title={"Upcoming on Netflix"} category={"upcoming"} />
         <Titlecards title={"Top Pics for You"} category={"now_playing"} />
       </div>
       <Footer />
